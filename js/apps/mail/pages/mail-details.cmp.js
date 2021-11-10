09a663020f0c62334b457f4dddeab366979383c9
Nofar Alon <nofaralon28@gmail.com>
2021-11-10 22:45:57 +0200

    adding filter

--- js/apps/mail/pages/mail-details.cmp.js
+++ js/apps/mail/pages/mail-details.cmp.js
@@ -1,4 +1,4 @@
-// import { mailsService } from '../apps/mail/service/mail-service.js';
+import { mailsService } from '../../mail/service/mail-service.js';
 // import mailList from '../apps/mail/cmps/mail-list.cmp.js'
 // import mailFilter from '../apps/mail/cmps/mail-filter.cmp.js'
 
@@ -7,7 +7,13 @@
 export default {
     template: `
     <section v-if="mail" class="mail-details">
-        <div>hi</div>
+        <div>{{mail.subject}}</div>
+        <div>{{mail.to}}</div>
+        <div>{{mail.body}}</div>
+        <div>{{mail.sentAt}}</div>
+        </div>
+
+        <button @click="goToList" >Go Back</button>
     </section>
     `,
     data() {
@@ -16,11 +22,14 @@ export default {
         }
     },
     created() {
-        // const { mailId } = this.$route.params;
-        // mailsService.getById(mailId)
-        //     .then(mail => this.mail = mail);
+        const { mailId } = this.$route.params;
+        mailsService.getById(mailId)
+            .then(mail => this.mail = mail);
     },
     methods: {
+        goToList() {
+            this.$router.push('/mail')
+        }
 
 
 
