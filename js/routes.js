6aebb850f0fcc66b8939813dff17ac1946d433bf
Nofar Alon <nofaralon28@gmail.com>
2021-11-10 19:39:05 +0200

    add filter

--- js/routes.js
+++ js/routes.js
@@ -14,6 +14,10 @@ const routes = [{
         component: mailApp
     },
     {
+        path: '/mail/:mailId',
+        component: mailDetails
+    },
+    {
         path: '/keep',
         component: keepApp
     }
